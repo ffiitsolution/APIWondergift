5f9b39146c2996c9bbcdf4f9796fb06752121f9b
ffiitsolution <125252897+ffiitsolution@users.noreply.github.com>
2023-12-01 13:34:57 +0700

    Add files via upload

---
+++ APIWondergift/src/test/java/com/api/ffi/wondergift/WondergiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ffi.wondergift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WondergiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
